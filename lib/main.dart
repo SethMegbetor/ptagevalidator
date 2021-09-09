2821c13186e71521fa99711f85115316bcd5c9b0
SethMegbetor <sethmegbetor@gmail.com>
2021-09-09 11:04:31 +0000

    finished project

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 
 void main() {
@@ -32,19 +30,57 @@ class Age extends StatefulWidget {
 
 class _AgeState extends State<Age> {
   String _message = "";
+  late String _name;
+  late int _age;
+
+  void nameOnSubmitted(String value) {
+    _name = value;
+  }
+
+  void ageOnSubmitted(String value) {
+    try {
+      _age = int.parse(value);
+    } on FormatException catch (e) {
+      setState(() {
+        _message = 'please enter a valid age';
+      });
+    }
+  }
+
+  void enterClub() {
+    setState(() {
+      if (_age >= 18) {
+        _message = "Hello $_name, You are allowed entry";
+      } else {
+        _message = "$_name, you are not old enough";
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.all(16),
+      padding: EdgeInsets.all(42),
       child: Column(
         children: <Widget>[
           Text(
             _message,
-            style: TextStyle(fontSize: 20),
+            style: TextStyle(fontSize: 24),
           ),
           TextField(
             decoration: InputDecoration(hintText: "Enter your name"),
+            onChanged: nameOnSubmitted,
+          ),
+          TextField(
+            decoration: InputDecoration(hintText: 'Enter your age'),
+            onChanged: ageOnSubmitted,
+          ),
+          RaisedButton(
+            child: Text(
+              "Submit",
+              style: TextStyle(fontSize: 24),
+            ),
+            onPressed: enterClub,
           )
         ],
       ),
